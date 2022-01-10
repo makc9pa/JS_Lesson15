054a47ba0239b900abe221846ecc5b8f8d06417d
makc9pa <2max2@mail.ru>
2022-01-10 23:16:38 +0300

    feat(lesson15): оптимизщировал метод createElement, убрал дублирование кода

--- script.js
+++ script.js
@@ -11,25 +11,21 @@ const DomElement = function (name, height, width, bg, fontSize) {
         if (name.slice(0, 1) === '.') {
             elem = document.createElement('div')
             elem.className = name.slice(1, name.length)
-            let node = document.createTextNode('Новый класс')
-            elem.appendChild(node)
-            document.body.appendChild(elem)
+            elem.innerText = 'Новый класс'
         }
 
         if (name.slice(0, 1) === '#') {
             elem = document.createElement('p')
             elem.id = name.slice(1, name.length)
-            let node = document.createTextNode('Новый параграф')
-            elem.appendChild(node)
-            document.body.appendChild(elem)
+            elem.innerText = 'Новый параграф'
         }
 
+        document.body.appendChild(elem)
         elem.style.cssText = `
         height: ${this.height};
         width: ${this.width};
         background-color: ${this.bg};
-        font-size: ${this.fontSize};
-        `
+        font-size: ${this.fontSize};`
     }
 }
 
